b2a8bfda22f5d24f4f66039423f0c56734897e31
Steven Braeger <steve@soapforge.com>
2014-08-28 18:05:11 -0400

    Update diddit.py

--- diddit.py
+++ diddit.py
@@ -9,10 +9,11 @@ class Dependency(ndb.Model):
 	active=ndb.BooleanProperty(default=True,indexed=True)
 	depends_on=ndb.KeyProperty(indexed=True,repeated=True)
 	visible=ndb.BooleanProperty(default=True,indexed=True)
-	status=ndb.StringProperty(indexed=True,default="False") 
+	taskclass=ndb.JsonProperty(indexed=True,default="False") 
 	#status is the name of a member-function (js or python) to call and a set of arguments in json
-	creation_date=ndb.DateTimeProperty(
-	comparator=ndb.StringProperty() #comparator is the js-function to use to compare.  Default topo-sort
+	creation_timestamp=ndb.DateTimeProperty(auto_add=True)
+	last_updated_timestamp=ndb.DateTimeProperty(auto_add_now=True)
+	#comparator=ndb.StringProperty() #comparator is the js-function to use to compare.  Default topo-sort
 	
 class User(ndb.Model):
 	groups=ndb.KeyProperty(indexed=True,repeated=True)
