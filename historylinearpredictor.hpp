7b3ff6bcbe6a75507663105580ef7c52655a7b8c
Steven Braeger <steve@soapforge.com>
2017-05-09 18:36:39 -0400

    Update historylinearpredictor.hpp

--- historylinearpredictor.hpp
+++ historylinearpredictor.hpp
@@ -1,3 +1,13 @@
+#include<Eigen/Dense>
+#include<Eigen/Cholesky>
+#include<vector>
+#include<algorithm>
+
+
+//http://n.schraudolph.org/pubs/ChiTruSch08.pdf
+//https://en.wikipedia.org/wiki/Vector_autoregression
+//https://en.wikipedia.org/wiki/Levinson_recursion
+//https://en.wikipedia.org/wiki/Autoregressive_model
 
 class CircularHistoryBase
 {
@@ -5,61 +15,60 @@ protected:
 	void history_append(const double* input,size_t num_to_append)
 	{
 		double* hqb=history_queue.data();
-		double* tgt=hqb+(num_history-1)*num_inputs;
-		double* src=tgt-1;
-		num_to_append=num_to_append > num_history ? num_history : num_to_append;
-		num_history*=num_inputs;
-		num_to_append*=num_inputs;
-		for(size_t i=num_to_append;i<num_history;i++)
-		{
-			*(tgt--)=*(src--);
-		}
-		std::copy(input,input+num_to_append,hqb);
+		num_to_append = num_to_append > num_history ? num_history : num_to_append;
+		std::rotate(hqb,hqb+(num_history-num_to_append)*num_inputs,hqb+num_history*num_inputs);
+		std::copy(input,input+num_to_append*num_inputs,hqb);
 	}
 	
-	std::VectorXd history_queue;
+	Eigen::VectorXd history_queue;
 public:
 	size_t num_history;
 	size_t num_outputs;
 	size_t num_inputs;
 	
-	HistoryLinearPredictor(size_t nh,size_t no,size_t ni):
+	CircularHistoryBase(size_t nh,size_t no,size_t ni):
 		num_history(nh),
 		num_outputs(no),
 		num_inputs(ni),
-		history_queue(nh*ni,1),
+		history_queue(nh*ni,1)
 	{}
+};
+
+
+class HistoryLinearPredictor: public CircularHistoryBase
+{
+public:
+	Eigen::MatrixXd coefficients;	//should this design make the coeffs transposed?  Yes.
+	HistoryLinearPredictor(size_t nh,size_t no,size_t ni,const Eigen::MatrixXd& c):
+		CircularHistoryBase(nh,no,ni),
+		coefficients(c)
+	{}
+	void update(const double* input,size_t num_inputs_array=1)
+	{
+		history_append(input,num_inputs_array);
+	}
+	Eigen::VectorXd predict() const
+	{
+		return coefficients*history_queue;
+	}
+	void restart()
+	{
+		memset(history_queue.data(),0,sizeof(double)*history_queue.rows());
+	}
 };
 
 class HistoryLinearModel: public CircularHistoryBase
 {
 private:
-	LDL<Eigen::MatrixXd,double> ldl;
+	Eigen::LDLT<Eigen::MatrixXd> ldl;
 	
 public:
-	class Predictor: public CircularHistoryBase
-	{
-	public:
-		MatrixXd coefficients;	//should this design make the coeffs transposed?  Yes.
-		HistoryLinearPredictor(size_t nh,size_t no,size_t ni,const MatrixXd& c=MatrixXd(no,ni*nh))
-			CircularHistoryBase(nh,no,ni)
-		{}
-		void update(const double* input,size_t num_inputs_array=1)
-		{
-			history_append(input,num_inputs_array);
-		}
-		VectorXd predict() const
-		{
-			return coefficients*history_queue;
-		}
-	};
-
 	Eigen::MatrixXd lefttracker;
 	
 	HistoryLinearModel(size_t nh,size_t no,size_t ni):
 		CircularHistoryBase(nh,no,ni),
 		ldl(nh*ni),
-		lefttracker(MatrixXd::Zero(ni*nh,no))
+		lefttracker(Eigen::MatrixXd::Zero(ni*nh,no))
 	{
 		ldl.setZero();
 	}
@@ -67,15 +76,13 @@ public:
 	void train(const double* output,const double* input)
 	{
 		history_append(input,1);
-		Eigen::Map<MatrixXd> outpm(output,1,num_outputs);
+		Eigen::Map<const Eigen::MatrixXd> outpm(output,1,num_outputs);
 		
 		lefttracker+=history_queue*outpm;
 		ldl.rankUpdate(history_queue);
 	}
 	HistoryLinearPredictor predictor() const
 	{
-		return HistoryLinearPredictor(num_history,num_outputs,num_inputs,ldl.solve(lefttracker).transposed());
+		return HistoryLinearPredictor(num_history,num_outputs,num_inputs,ldl.solve(lefttracker).transpose());
 	}
 };
-
-
